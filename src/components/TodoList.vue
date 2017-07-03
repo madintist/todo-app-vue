37df65e8445c2581c206ce6e1f6955dd84a90d4a
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-07-03 15:44:11 -0500

    Added Bootstrap HTML to TodoList

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,4 +1,11 @@
-<template></template>
+<template>
+  <ul class="list-group">
+    <li class="list-group-item">1</li>
+    <li class="list-group-item">2</li>
+    <li class="list-group-item">3</li>
+    <li class="list-group-item">4</li>
+  </ul>
+</template>
 
 <script></script>
 
