317cd9477545749f958750ba08d75c24340a1ce2
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-07-03 16:05:19 -0500

    Added code to TodoListItem.vue

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -14,18 +14,6 @@
 </script>
 
 <style scoped>
-li {
-    background-color: #fff;
-    border: 1px solid #ddd;
-    display: block;
-    margin-bottom: -1px;
-    padding: 10px 15px;
-    position: relative;
-}
-li:first-child {
-    border-top-left-radius: 4px;
-    border-top-right-radius: 4px;
-}
 
 ul {
     margin-bottom: 20px;
--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,5 +1,29 @@
-<template></template>
+<template>
+  <li>{{ todoItem.title }}</li>
+</template>
 
-<script></script>
+<script>
+  export default {
+    name: 'todo-list-item',
+    props: [
+      'todo-item'
+    ]
+  }
+</script>
 
-<style scoped></style>
+<style scoped>
+li {
+    background-color: #fff;
+    border: 1px solid #ddd;
+    display: block;
+    margin-bottom: -1px;
+    padding: 10px 15px;
+    position: relative;
+}
+
+li:first-child {
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+}
+</style>
+
