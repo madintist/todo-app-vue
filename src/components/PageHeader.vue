87a3f5f5e07793bcf06ffc7130cdf52cdf5751e6
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-07-03 14:59:14 -0500

    Created PageHeader component

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,5 +1,25 @@
-<template></template>
+<template>
+  <h1>{{ headerText }}</h1>
+</template>
 
-<script></script>
+<script>
+  export default {
+    name: 'page-header',
+    props: [
+      'header-text'
+    ]
+  };
+</script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+    color: inherit;
+    font-family: inherit;
+    font-size: 36px;
+    font-weight: 500;
+    line-height: 1.1;
+    margin: 0.67em 0;
+    margin-bottom: 10px;
+    margin-top: 20px;
+}
+</style>
